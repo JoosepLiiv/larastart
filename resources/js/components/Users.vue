f0323081b0bff54e8b2e1e4a9a1e9256a317d1cf
Joosep <joosepliiv97@gmail.com>
2019-02-06 23:04:19 +0000

    vForm to add new user

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -58,11 +58,40 @@
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="form-group">
+                <input v-model="form.name" type="text" name="name" placeholder="Name"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                <has-error :form="form" field="name"></has-error>
+                </div>
+                <div class="form-group">
+                <input v-model="form.email" type="text" name="email" placeholder="Email Address"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                <has-error :form="form" field="email"></has-error>
+                </div>
+                <div class="form-group">
+                <textarea v-model="form.bio" type="text" name="bio" placeholder="Short bio for user (optional)"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                <has-error :form="form" field="bio"></has-error>
+                </div>
+                <div class="form-group">
+                <select name="type" v-model="form.type" id="type" class="form-control" :class="{
+                    'is-invalid': form.errors.has('type') }">
+                        <option value="">Select User Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">Standard User</option>
+                        <option value="author">Author</option>
+                    </select>
+                <has-error :form="form" field="type"></has-error>
+                </div>
+                <div class="form-group">
+                <input v-model="form.password" type="password" name="password" id="password" placeholder="Password"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                <has-error :form="form" field="password"></has-error>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary">Create</button>
             </div>
             </div>
         </div>
@@ -73,6 +102,18 @@
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
