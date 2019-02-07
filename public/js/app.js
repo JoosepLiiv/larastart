86b12ec1c64a8a11b761f5f1e2991c8e77ef73b0
Joosep <joosepliiv97@gmail.com>
2019-02-07 21:33:18 +0000

    User model and controller; adding user

--- public/js/app.js
+++ public/js/app.js
@@ -38384,7 +38384,7 @@ var staticRenderFns = [
       _vm._v(" "),
       _c(
         "button",
-        { staticClass: "btn btn-primary", attrs: { type: "button" } },
+        { staticClass: "btn btn-primary", attrs: { type: "submit" } },
         [_vm._v("Create")]
       )
     ])
