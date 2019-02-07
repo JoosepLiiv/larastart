9da4a30fb06e8832e95bd1ef9c2ded34f96f9ea8
Joosep <joosepliiv97@gmail.com>
2019-02-07 23:12:54 +0000

    Sweetalert and toaster

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,19 @@ require('./bootstrap');
 window.Vue = require('vue');
 import { Form, HasError, AlertError } from 'vform';
 import moment from 'moment';
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+  });
+
+window.Toast = Toast;
+
 import VueProgressBar from 'vue-progressbar';
 
 const options = {
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -127,6 +127,13 @@
             createUser(){
                 this.$Progress.start();
                 this.form.post('api/user');
+
+                $('#addNewModal').modal('hide');
+
+                Toast.fire({
+                    type: 'success',
+                    title: 'User has been created successfully!'
+                });
                 this.$Progress.finish();
             }
         },
