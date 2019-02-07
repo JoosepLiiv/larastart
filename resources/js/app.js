c90bfcf42d803940d240a9599ce804419ef51e89
Joosep <joosepliiv97@gmail.com>
2019-02-07 22:53:29 +0000

    Adding progressbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,23 @@ require('./bootstrap');
 window.Vue = require('vue');
 import { Form, HasError, AlertError } from 'vform';
 import moment from 'moment';
+import VueProgressBar from 'vue-progressbar';
+
+const options = {
+    color: '#bffaf3',
+    failedColor: '#874b4b',
+    thickness: '3px',
+    transition: {
+      speed: '0.2s',
+      opacity: '0.6s',
+      termination: 300
+    },
+    autoRevert: true,
+    location: 'top',
+    inverse: false
+  }
+
+  Vue.use(VueProgressBar, options)
 
 window.Form = Form;
 Vue.component(HasError.name, HasError)
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -125,7 +125,9 @@
             },
 
             createUser(){
+                this.$Progress.start();
                 this.form.post('api/user');
+                this.$Progress.finish();
             }
         },
         created() {
